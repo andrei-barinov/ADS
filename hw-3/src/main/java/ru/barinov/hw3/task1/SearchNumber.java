be99378dbc86c19de91303441ded9f90cc11a886
Andre <ncl8@mail.ru>
2022-04-05 18:44:49 +0300

    hw-3.2

--- hw-3/src/main/java/ru/barinov/hw3/task1/SearchNumber.java
+++ hw-3/src/main/java/ru/barinov/hw3/task1/SearchNumber.java
@@ -1,4 +1,37 @@
 package main.java.ru.barinov.hw3.task1;
 
-public class SearchNamber {
-}
+public class SearchNumber {
+
+    public static int search(Integer[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        int base;
+        int n = 0;
+
+
+        if(arr.length == 0){
+            return n = 1;
+        } else if(arr[end] - end == 2 && arr[0] != 1) {
+            return  n = arr[start] - 1;
+        }else if(arr[end] - end == 1 && arr[0] == 1){
+            return n = arr[end] + 1;
+        } else{
+            while(true){
+                base = (end + start) / 2;
+                if (arr[base] - arr[base - 1] == 2 || (arr[base + 1] - arr[base] == 2)) {
+                    if (arr[base] - arr[base - 1] == 2){
+                        n = arr[base] - 1;
+                    } else {
+                        n = arr[base] + 1;
+                    }
+                    break;
+                } else if(arr[start] - start == 1 && arr[base] - base == 2){
+                    end = base - 1;
+                }else {
+                    start = base + 1;
+                }
+                }
+            }
+        return  n;
+        }
+    }
--- hw-3/src/main/java/ru/barinov/hw3/task1/TestSearchNumber.java
+++ hw-3/src/main/java/ru/barinov/hw3/task1/TestSearchNumber.java
@@ -1,4 +1,39 @@
 package main.java.ru.barinov.hw3.task1;
 
+import java.util.Arrays;
+
 public class TestSearchNumber {
+    public static void main(String[] args) {
+//        Integer[] arr = {};
+//        System.out.println("Пропущено: 1");
+//        System.out.println("Найдено пропущенное число: " + SearchNumber.search(arr));
+
+//        Integer[] arr = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+//        System.out.println("Пропущено: 1");
+//        System.out.println("Найдено пропущенное число: " + SearchNumber.search(arr));
+
+//        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+//        System.out.println("Пропущено: 18");
+//        System.out.println("Найдено пропущенное число: " + SearchNumber.search(arr));
+
+//        Integer[] arr = {1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+//        System.out.println("Пропущено: 5");
+//        System.out.println("Найдено пропущенное число: " + SearchNumber.search(arr));
+
+//        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20};
+//        System.out.println("Пропущено: 18");
+//        System.out.println("Найдено пропущенное число: " + SearchNumber.search(arr));
+
+        Integer[] arr = new Integer[10000];
+        for (int i = 0; i < 10000; i++){
+            if (i >= 7753){
+                arr[i] = i + 2;
+            } else {
+            arr[i] = i + 1;
+            }
+        }
+        System.out.println("Пропущено: 7754");
+        System.out.println("Найдено пропущенное число: " + SearchNumber.search(arr));
+
+    }
 }
